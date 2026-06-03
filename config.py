5d705fee0c26c96cc62dede8e81bdc1626fcf6e9
sivaprasad <sivaprasad@nuevesolutions.in>
2026-06-03 23:54:08 +1400

    chore: scaffold project structure and dependencies

---
+++ config.py
@@ -0,0 +1,8 @@
+OLLAMA_BASE_URL = "http://localhost:11434"
+EMBED_MODEL = "nomic-embed-text"
+CHAT_MODEL = "llama3.2"
+CHUNK_SIZE = 500
+CHUNK_OVERLAP = 50
+TOP_K = 4
+CHROMA_PATH = "./chroma_db"
+DOCS_PATH = "./docs"
